f0fbd553a55bc6f1c55dd2c4a41c08fb00d88100
Juan <jao9@alu.ua.es>
2022-02-16 20:21:09 +0100

    Añadidos los cambios en Program.cs

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,45 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            string continuar;
+            do
+            {
+                string respuesta;
+                string numero;
+                double valor;
+                Console.WriteLine("Que unidad quieres convertir Segundos o Minutos? (s/m)");
+                respuesta = Console.ReadLine();
+                if (respuesta=="s") {
+                    Console.WriteLine("Ahora introduce el valor que deseas cambiar >=0");
+                    numero = Console.ReadLine();
+                    valor = double.Parse(numero);
+                    Console.WriteLine(HadaP1.Seconds2Minutes(valor));
+                }
+                else
+                {
+                    if (respuesta=="m")
+                    {
+                        Console.WriteLine("Ahora introduce el valor que deseas cambiar >=0");
+                        numero = Console.ReadLine();
+                        valor = double.Parse(numero);
+                        Console.WriteLine(HadaP1.Minutes2Seconds(valor));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entrad incorrecta");
+                    }
+                }
+                do
+                {
+                    Console.WriteLine("Quieres continuar?(s/n)");
+                    continuar = Console.ReadLine();
+                    if(continuar != "s" || continuar != "n")
+                    {
+                        Console.WriteLine("Entrada incorrecta");
+                    }
+                } while (continuar!="s" || continuar!="n");
+
+            } while (continuar=="s");
         }
     }
 }
