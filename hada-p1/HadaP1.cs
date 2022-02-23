02979ff43cd93fe6656a2c9f4bdd2eef8c67df80
Juan <jao9@alu.ua.es>
2022-02-23 19:04:50 +0100

    Hacer commit de cambios "staged"

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace hada_p1
 {
+    /// <summary>
+    /// esa clase contiene métodos para convertir de
+    /// segundos a minutos y viceversa
+    /// </summary>
     class HadaP1
     {
+        /// <summary>
+        /// Este método convierte segundos a minutos
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static double Seconds2Minutes(double s){
 
             if (s != 0){
@@ -19,6 +29,11 @@ namespace hada_p1
             return s;
         }
 
+        /// <summary>
+        /// Este método convierte minutos a segundos
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
         public static double Minutes2Seconds(double m){
 
             m = m * 60;
